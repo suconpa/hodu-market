8ddfe8835c96b2a7ccf497ad493f104cbd377d8a
이수아 <isua@isuas-MacBook-Air.local>
2023-09-07 21:58:32 +0900

    Chore: 초기 세팅

---
+++ src/Router/Router.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from 'App';
+import Home from 'Pages/Home';
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default Router;
